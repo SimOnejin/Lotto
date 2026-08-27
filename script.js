cd9c30ae9ed031b528c49a3f5fcf5a4d9c8f16d2
SimOnejin <134303424+SimOnejin@users.noreply.github.com>
2026-08-27 09:55:55 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const themeBtn = document.getElementById("themeToggle");
+
+function loadTheme(){
+    const savedTheme = localStorage.getItem("theme");
+
+    if(savedTheme === "dark"){
+        document.body.classList.add("dark");
+        themeBtn.textContent = "☀️ 라이트모드";
+    }
+}
+
+themeBtn.addEventListener("click",()=>{
+    document.body.classList.toggle("dark");
+
+    const isDark =
+        document.body.classList.contains("dark");
+
+    if(isDark){
+        localStorage.setItem("theme","dark");
+        themeBtn.textContent="☀️ 라이트모드";
+    }else{
+        localStorage.setItem("theme","light");
+        themeBtn.textContent="🌙 다크모드";
+    }
+});
+
+loadTheme();
